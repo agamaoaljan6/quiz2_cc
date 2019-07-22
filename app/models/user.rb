871a5e3edca8dd93712298f0201541d3676fdf91
Aljan Agamao <agamaoaljan@gmail.com>
2019-07-22 12:32:03 -0700

    added associations for likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
     has_many :reviews
     has_secure_password
 
+    has_many :ideas, dependent: :nullify
+    has_many :reviews, dependent: :nullify
+    has_many :likes, dependent: :destroy
+    has_many :liked_ideas, through: :likes, source: :idea
+    
     validates(
         :username, 
         presence: true,
